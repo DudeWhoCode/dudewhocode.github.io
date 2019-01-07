fe288e5a3423a00b6d1df5dc311642e571850b43
Naren <narencodes@gmail.com>
2019-01-07 16:38:38 +0530

    Initial commit

---
+++ home/testimonials.md.d
@@ -0,0 +1,26 @@
+---
+title: "Testimonials"
+date: 2019-01-07T09:04:53+05:30
+draft: true
+---
+
+<div class="user-details">
+    <div class="testi-container">
+        <div class="font">
+            <p align="left"><span>Naresh Kumar Pidikiti.</span> Founder &amp; CEO at Siliconicxs Industrial Solutions.<a href="https://www.linkedin.com/in/naresh-kumar-p-95a6741a/"><i class="fa fa-linkedin-square fa-3x linkedin-container" aria-hidden="true" align="right"></i></a></p>
+            <p align="left"><i class="fa fa-quote-left fa-3x fa-pull-left fa-border" aria-hidden="true"></i><i>A smart worker, always with a curiosity to explore technology. He built complex systems with simple interfaces.</i></p>
+        </div>
+    </div>
+    <div class="testi-container">
+        <div class="font">
+            <p align="left"><span>Senthil Velan.</span> Founder &amp; CEO at NilaTech.<a href="https://www.linkedin.com/in/jsvelan/"><i class="fa fa-linkedin-square fa-3x linkedin-container" aria-hidden="true" align="right"></i></a></p>
+            <p align="left"><i class="fa fa-quote-left fa-3x fa-pull-left fa-border" aria-hidden="true"></i><i>Naren helped us to showcase multiple prototypes for our client visits. His knack for rapid prototyping always amazes me.</i></p>
+        </div>
+    </div>
+    <div class="testi-container">
+        <div class="font">
+            <p align="left"><span>Saravana Kumar.</span> Senior Technical Lead at Dot Solved Systems Inc.<a href="https://www.linkedin.com/in/saravanakumar-karunanithi-a5bb8b10/"><i class="fa fa-linkedin-square fa-3x linkedin-container" aria-hidden="true" align="right"></i></a></p>
+            <p align="left"><i class="fa fa-quote-left fa-3x fa-pull-left fa-border" aria-hidden="true"></i><i>I have worked with Naren in many projects at scale. Here are some words I would use to describe him: Result oriented, Vision focused, quality and timely delivery and most importantly consistency.</i></p>
+        </div>
+    </div>
+</div>

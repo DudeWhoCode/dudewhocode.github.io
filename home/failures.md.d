2dfe7456bdf4d939a8d692154e74d095a0b81fcd
Naren <narencodes@gmail.com>
2019-01-21 18:55:29 +0530

    More information

---
+++ home/failures.md.d
@@ -0,0 +1,14 @@
+---
+title: "Failures"
+draft: true
+weight: 0
+---
+
+
+> Life before death.. Strength before weakness.. Journey before destination..    
+> - Brandon Sanderson, Way of Kings
+
+I recently came across this quote in a book and I can't take it out of my mind. When ever I think about my failures I just repeat this quote inside my mind. It simply means destinations are always there waiting for us, it's about the journey you take. And the quick failures are one of the solid way to do course correction towards the destination.
+
+#### Blogging
+I started blogging 5 years back. I blogged at narendran.co.in(now some one else owns it), backendhorror.com, getsetgolang.com (I think I let the domain name expire). Then [here](https://example.com) and then now moving everything to [medium](https://www.medium.com/@dudewhocode).
